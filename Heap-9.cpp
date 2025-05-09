c573a55889bdef817efdca29b6026016f16a5ee6
Abhishek <abhishekchaudhari1021@gmail.com>
2025-05-09 21:25:25 +0530

    UPDATED FILES

---
+++ Heap-9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int count = 0, n; // Global variable to count the number of nodes in the heap tree
+int key[20];
+// Class to represent Heap tree 
+class HeapTree {
+public:
+    int data; // Data of the node
+
+    void accept() { // Function to accept data for the node
+        cout << "Enter number of values: ";
+        cin >> n; // Accept the number of values
+    }
+
+    void BuildHeap(int key[], int n, int ){
+        
+    }
+};
--- hash-table-7.cpp
+++ hash-table-7.cpp
@@ -35,34 +35,42 @@ void studentRecords::Table()
 
 void studentRecords::accept()
 {
-    int loc, ch;
+    int loc;
     for (int i = 0; i < n; i++)
     {
-        loc = key[i] % 10;
-        if (h[loc].ID == -1)
+        loc = key[i] % 10;  // Calculate original hashed location
+
+        if (h[loc].ID == -1)  // If the location is free, insert normally
         {
             cout << "Enter Name for ID " << key[i] << " : ";
             cin >> h[loc].Name;
             h[loc].ID = key[i];
+            chain[loc] = -1;  // No collision
             cout << "Record Added at location " << loc << "." << endl;
         }
         else
         {
             cout << "\nCollision Occurred at location " << loc << " !!!" << endl;
-            loc = (loc + 1) % 10;
-            while (h[loc].ID != -1)
+            int newLoc = (loc + 1) % 10;
+
+            // Find next available position
+            while (h[newLoc].ID != -1)
             {
-                loc = (loc + 1) % 10;
+                newLoc = (newLoc + 1) % 10;
             }
+
             cout << "Enter Name for ID " << key[i] << " : ";
-            cin >> h[loc].Name;
-            h[loc].ID = key[i];
-            cout << "Record Added at location " << loc << "." << endl;
-            h[i].change = loc;
-            chain[i] = loc;
+            cin >> h[newLoc].Name;
+            h[newLoc].ID = key[i];
+            cout << "Record Added at location " << newLoc << "." << endl;
+
+            // Update change field at the original location
+            chain[loc] = newLoc;
+            h[loc].change = newLoc;
         }
     }
 }
+
 
 void studentRecords::display()
 {
@@ -148,12 +156,40 @@ void studentRecords::deleteRecord()
                 delLoc = i;
                 cout << "\nRecord found at location " << delLoc << ": ID = " << h[delLoc].ID << ", Name = " << h[delLoc].Name << endl;
 
-                // Delete record
+                // Delete the record
                 h[delLoc].ID = -1;
                 h[delLoc].Name = "";
                 h[delLoc].change = -1;
+                chain[delLoc] = -1;
                 cout << "Record deleted successfully!" << endl;
 
+                // Check if the deleted location had a chained element
+                int movedLoc = chain[delLoc];
+                while (movedLoc != -1)  // Process every displaced entry
+                {
+                    int expectedLoc = key[movedLoc] % 10;
+
+                    if (h[expectedLoc].ID == -1) // Move back to expected position if free
+                    {
+                        h[expectedLoc].ID = h[movedLoc].ID;
+                        h[expectedLoc].Name = h[movedLoc].Name;
+                        h[expectedLoc].change = chain[movedLoc];  // Preserve chain links
+
+                        // Clear old location
+                        h[movedLoc].ID = -1;
+                        h[movedLoc].Name = "";
+                        h[movedLoc].change = -1;
+                        chain[movedLoc] = -1;
+
+                        cout << "Record restored to original location: " << expectedLoc << endl;
+                    }
+                    else
+                    {
+                        cout << "Expected location occupied, chain maintained.\n";
+                    }
+
+                    movedLoc = chain[movedLoc]; // Move to next in chain
+                }
                 break;
             }
         }
@@ -162,38 +198,13 @@ void studentRecords::deleteRecord()
         {
             cout << "Record not found!" << endl;
         }
-        else
-        {
-            // Resolve collisions and maintain chain properly
-            for (int i = 0; i < 10; i++)
-            {
-                if (h[i].change == delLoc)
-                {
-                    int correctLoc = key[i] % 10;
-
-                    if (h[correctLoc].ID == -1) // If original spot is empty, move it back
-                    {
-                        h[correctLoc].ID = h[i].ID;
-                        h[correctLoc].Name = h[i].Name;
-                        h[i].ID = -1;
-                        h[i].Name = "";
-                        h[i].change = -1;
-                        chain[i] = -1;
-                        cout << "Record moved to its original location at " << correctLoc << endl;
-                    }
-                    else
-                    {
-                        // Update chain properly
-                        h[i].change = h[i].change == delLoc ? -1 : h[i].change;
-                    }
-                }
-            }
-        }
 
         cout << "\nDo you want to delete another record? (Yes - 1 / No - 0): ";
         cin >> ch;
     } while (ch != 0);
 }
+
+
 
 int main()
 {
